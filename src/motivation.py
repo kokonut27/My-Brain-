021930953004bde86002554ce86097c96cde0e58
Glitch <76911308+JBYT27@users.noreply.github.com>
2021-05-13 14:14:16 -0400

    Create motivation.py
    ikr. awesome. :)

---
+++ src/motivation.py
@@ -0,0 +1,10 @@
+import os, time
+
+yn = input("Do you have motivation? ")
+
+if yn[0] == "y":
+  print("You didnt blow up :0")
+elif yn[0] == "n":
+  print("You blew up :0")
+else:
+  print("Your math teacher gave you a f-")
